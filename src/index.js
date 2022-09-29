aea1aa646ca43cc11c0e428a4ecb32f978931307
Subramanya K S <subramanyaks2205@gmail.com>
2022-09-29 19:45:02 +0530

    added index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Main from '../src/components/main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
