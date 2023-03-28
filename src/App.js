9ac05bdb442514ad000be6aeffa08216145d0cdc
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-28 23:05:42 +0530

    refactored  code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/main';
 import { ThemeProvider } from './context/ThemeContext';
+import WeatherContext from './context/weatherContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider>
+        <WeatherContext>
         <Header/>
         <Main/>
+        </WeatherContext>
       </ThemeProvider>
       
     </div>
