28c143df9cc90698f1409e1f7428953c0a9e09cc
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-28 22:08:22 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Header from './components/Header';
 import Main from './components/main';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      <ThemeProvider>
+        <Header/>
+        <Main/>
+      </ThemeProvider>
+      
     </div>
   );
 }
