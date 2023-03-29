af9834415e7f03df4325a403d2dcc0b7bc5a276d
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-29 06:50:30 +0530

    updated MiniCards

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -1,13 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
+import React, { useContext } from 'react'
 import { Card } from 'react-bootstrap';
+import './main.css'
+import { Theme } from '../context/ThemeContext';
 
-const MiniCard = () => {
+const MiniCard = ({icon,text,cdata,unit}) => {
+  const {dark} = useContext(Theme);
   return (
-    <Card style={{ width: "20%" }}>
+    <Card className={dark?"card-dark":"card-light"} style={{ width: "20%" }}>
         <Card.Text>
-          <FontAwesomeIcon icon={faWind} flip /> <b>Wind Speed: </b>{" "}
-          {data.wind_speed} km/hr
+          <FontAwesomeIcon icon={icon} fade /> <b>{text} </b>{" "}
+          {cdata} {unit}
         </Card.Text>
     </Card>
   )
