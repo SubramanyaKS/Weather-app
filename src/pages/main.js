203d6fd71c396198c842b575d1e744e793f25431
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-25 22:30:27 +0530

    updated

--- src/pages/main.js
+++ src/pages/main.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherCard from "../components/WeatherCard";
 import { Theme } from "../context/ThemeContext";
 import { Weather } from "../context/weatherContext";
+import { fetching } from "../util/fetching";
 //import dotenv from  'dotenv'
 
 const Main = () => {
@@ -19,37 +20,26 @@ const Main = () => {
     //const env = dotenv.config().parsed;
     event.preventDefault();
     console.log(state);
-    // console.log("ID",process.env.REACT_APP_API_KEY)
-
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${state}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((result) => {
-        setData({
-          id: result.id,
-          name: result.name,
-          country: result.sys.country,
-          lat: result.coord.lat,
-          lon: result.coord.lon,
-          temp: result.main.temp,
-          feels_like: result.main.feels_like,
-          min: result.main.temp_min,
-          max: result.main.temp_max,
-          pressure: result.main.pressure,
-          humidity: result.main.humidity,
-          wind_speed: result.wind.speed,
-          icon: result.weather[0].icon,
-          main: result.weather[0].description,
-        });
-
-        if (typeof result === typeof data) {
-          console.log("Subbu", result);
-        }
-        console.log(typeof result);
-        console.log(data.main);
-      })
-      .catch((error) => {
+    
+    fetching(state).then((result) => {
+      // console.log("Result",result.id);
+      setData({
+        id: result.id,
+        name: result.name,
+        country: result.sys.country,
+        lat: result.coord.lat,
+        lon: result.coord.lon,
+        temp: result.main.temp,
+        feels_like: result.main.feels_like,
+        min: result.main.temp_min,
+        max: result.main.temp_max,
+        pressure: result.main.pressure,
+        humidity: result.main.humidity,
+        wind_speed: result.wind.speed,
+        icon: result.weather[0].icon,
+        main: result.weather[0].description,
+      });
+    }).catch((error) => {
         console.log(error);
       });
   };
