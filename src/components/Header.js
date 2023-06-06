f1001b12e5a2f0e22481fc7246c98f1e8aa73603
Subramanya K S <subramanyaks2205@gmail.com>
2023-06-06 07:28:53 +0530

    updated toggle button using react-switch

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { Button, Container, Navbar } from 'react-bootstrap'
 import { Theme } from '../context/ThemeContext'
 import "../assets/main.css";
+import Switch from "react-switch";
 
 const Header = () => {
     const{dark,setDark} = useContext(Theme);
@@ -9,7 +10,8 @@ const Header = () => {
     <Navbar bg={dark?"dark":"light"} variant={dark?"dark":"light"}>
         <Container>
           <Navbar.Brand href="#home">Weather App</Navbar.Brand>
-          <Button className="theme-button" variant={dark?"light":"dark"} onClick={()=>setDark((dark)=>!dark)}>{dark?"Light":"Dark"}</Button>
+          {/* <Button className="theme-button" variant={dark?"light":"dark"} onClick={()=>setDark((dark)=>!dark)}>{dark?"Light":"Dark"}</Button> */}
+          <Switch checked={dark} onHandleColor="#000" offColor='#000' onColor="#fff" checkedIcon={false}  uncheckedIcon={false} onChange={()=>setDark((dark)=>!dark)}/>
         </Container>
       </Navbar>
   )
