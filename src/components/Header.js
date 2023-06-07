3f470e7725155ddc09f0259d9558608712fefa43
Subramanya K S <subramanyaks2205@gmail.com>
2023-06-07 22:57:09 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,6 @@ const Header = () => {
     <Navbar bg={dark?"dark":"light"} variant={dark?"dark":"light"}>
         <Container>
           <Navbar.Brand href="#home">Weather App</Navbar.Brand>
-          {/* <Button className="theme-button" variant={dark?"light":"dark"} onClick={()=>setDark((dark)=>!dark)}>{dark?"Light":"Dark"}</Button> */}
           <Switch checked={dark} onHandleColor="#000" offColor='#000' onColor="#fff" checkedIcon={false}  uncheckedIcon={false} onChange={()=>setDark((dark)=>!dark)}/>
         </Container>
       </Navbar>
