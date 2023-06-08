1ae7362a1f382955c9c734123b5dffab3b9d8575
Subramanya K S <subramanyaks2205@gmail.com>
2023-06-08 20:21:49 +0530

    seperated search bar from main

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,60 +1,30 @@
 import React, { useContext } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../assets/main.css";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Theme } from "../context/ThemeContext";
 import { Weather } from "../context/weatherContext";
-import { useWeatherData } from "../hook/useWeatherData";
 import WeatherDisplay from "../components/WeatherDisplay";
+import SearchBar from "../components/SearchBar";
 
 const Main = () => {
-  const {dark} = useContext(Theme);
-  const {data,state,setState} = useContext(Weather);
-  const { fetchWeatherData } = useWeatherData(state);
-  
-const handleChange = (value) => {
-    setState(value);
-  };
-  const fetchDetails = (event) => {
-    //const env = dotenv.config().parsed;
-    event.preventDefault();
-    console.log(state);
-    fetchWeatherData();
-  };
+  const { dark } = useContext(Theme);
+  const { data } = useContext(Weather);
+
   return (
     <>
-      <div className={dark?"main-dark":"main"}>
+      <div className={dark ? "main-dark" : "main"}>
         <br />
-        
-        <h1 className="headline"
-          style={dark?{  color: "#00ffff"}:{color:"#000"}}
+
+        <h1
+          className="headline"
+          style={dark ? { color: "#00ffff" } : { color: "#000" }}
         >
           Weather App
         </h1>
-        <div>
-        </div>
-        <div className="search">
-          <form onSubmit={fetchDetails}>
-            <div className="input-search">
-            <input
-            type="text"
-              placeholder="Enter the City"
-              icon={faSearch}
-              className="searchBar"
-              onChange={(e) => {
-                handleChange(e.target.value);
-              }}
-              id="outlined-basic"
-              variant="outlined"
-              label="Search"
-            />
-            <FontAwesomeIcon icon={faSearch} className="icon-switch"  style={dark?{ color:"#00ffff"}:{color:"#000"}} />
-            </div>
-          </form>
-        </div>
+        <div></div>
+        <SearchBar />
         <br />
-        <WeatherDisplay data={data}/>
+        <WeatherDisplay data={data} />
       </div>
     </>
   );
