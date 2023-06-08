67fa49b370ac6d96d55a28b524376f4b9225650d
Subramanya K S <subramanyaks2205@gmail.com>
2023-06-08 22:49:22 +0530

    modified url fetching

--- src/hook/useWeatherData.js
+++ src/hook/useWeatherData.js
@@ -1,45 +1,51 @@
-import { useContext} from "react";
+import { useContext } from "react";
 import { Weather } from "../context/weatherContext";
-export function useWeatherData(searchTerm) {
-    //using weather data context
-    const {setData} = useContext(Weather);
-    
-    // Function to fetch weather data from API
-    const fetchWeatherData = async () => {
-      if (searchTerm) {
-        try {
-  
-          // Call the OpenWeatherMap API with the search term
-          const response = await fetch(
-            `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
-          );
-          const result = await response.json();
-  
-          // Update the weather data state
-          setData({
-            id: result.id,
-            name: result.name,
-            country: result.sys.country,
-            lat: result.coord.lat,
-            lon: result.coord.lon,
-            temp: result.main.temp,
-            feels_like: result.main.feels_like,
-            min: result.main.temp_min,
-            max: result.main.temp_max,
-            pressure: result.main.pressure,
-            humidity: result.main.humidity,
-            wind_speed: result.wind.speed,
-            icon: result.weather[0].icon,
-            main: result.weather[0].description,
-          });
-        } catch (error) {
-          console.error('Error occurred while fetching weather data:', error);
-        
-        }
-      } else {
-        setData(null);
+
+/**
+ * Custom hook to fetch weather data from the API.
+ * @param {string} url - The URL for fetching weather data.
+ * @returns {Object} - An object containing the fetchWeatherData function.
+ */
+
+export function useWeatherData(url) {
+  // Accessing weather data context
+  const { setData } = useContext(Weather);
+
+  /**
+   * Function to fetch weather data from the API.
+   */
+  const fetchWeatherData = async () => {
+    if (url) {
+      try {
+        // Call the OpenWeatherMap API with the provided URL
+        const response = await fetch(url);
+        const result = await response.json();
+
+        // Update the weather data state
+        setData({
+          id: result.id,
+          name: result.name,
+          country: result.sys.country,
+          lat: result.coord.lat,
+          lon: result.coord.lon,
+          temp: result.main.temp,
+          feels_like: result.main.feels_like,
+          min: result.main.temp_min,
+          max: result.main.temp_max,
+          pressure: result.main.pressure,
+          humidity: result.main.humidity,
+          wind_speed: result.wind.speed,
+          icon: result.weather[0].icon,
+          main: result.weather[0].description,
+        });
+      } catch (error) {
+        console.error('Error occurred while fetching weather data:', error);
       }
-    };
-    return { fetchWeatherData };
-  }
-  
+    } else {
+      // Reset the weather data state
+      setData(null);
+    }
+  };
+
+  return { fetchWeatherData };
+}
