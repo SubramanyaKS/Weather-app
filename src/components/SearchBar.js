b1da7a51824603f6cec640a0ab005de76af27d0c
Subramanya K S <subramanyaks2205@gmail.com>
2023-08-14 23:38:48 +0530

    updated theme context

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,20 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useContext } from 'react'
-import { Weather } from '../context/weatherContext';
 import { useWeatherData } from '../hook/useWeatherData';
-import { Theme } from '../context/ThemeContext';
 import "../assets/main.css";
+import { WeatherDataContext } from '../context/weatherDataContext';
 
 const SearchBar = () => {
-    const {dark} = useContext(Theme);
-    const {state,setState} = useContext(Weather);
-  const { fetchWeatherData } = useWeatherData(`${process.env.REACT_APP_API_LINK}/weather?q=${state}&units=metric&appid=${process.env.REACT_APP_API_KEY}`);
+  const { state, dispatch } = useContext(WeatherDataContext);
+  const { fetchWeatherData } = useWeatherData(`${process.env.REACT_APP_API_LINK}/weather?q=${state.city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`);
     const handleChange = (value) => {
-        setState(value);
+        dispatch({ type: 'SET_CITY', payload: value });
       };
       const fetchDetails = (event) => {
         //const env = dotenv.config().parsed;
         event.preventDefault();
-        console.log(state);
+        // console.log(state.city);
         fetchWeatherData();
       };
   return (
@@ -35,7 +33,7 @@ const SearchBar = () => {
               variant="outlined"
               label="Search"
             />
-            <FontAwesomeIcon icon={faSearch} className="icon-switch"  style={dark?{ color:"#00ffff"}:{color:"#000"}} />
+            <FontAwesomeIcon icon={faSearch} className="icon-switch"  style={state.dark?{ color:"#00ffff"}:{color:"#000"}} />
             </div>
           </form>
         </div>
