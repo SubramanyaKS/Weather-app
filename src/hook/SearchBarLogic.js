a6dd74668036ae855da4fbb3bb3ceaec76756c38
Subramanya K S <subramanyaks2205@gmail.com>
2023-08-16 18:29:55 +0530

    seperate ui with logic

---
+++ src/hook/SearchBarLogic.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react'
+import { useWeatherData } from './useWeatherData';
+import { WeatherDataContext } from '../context/weatherDataContext';
+
+const useSearchBarLogic = () => {
+    const { state, dispatch } = useContext(WeatherDataContext);
+    const { fetchWeatherData } = useWeatherData(`${process.env.REACT_APP_API_LINK}/weather?q=${state.city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`);
+      const handleChange = (value) => {
+          dispatch({ type: 'SET_CITY', payload: value });
+        };
+        const fetchDetails = (event) => {
+          //const env = dotenv.config().parsed;
+          event.preventDefault();
+          // console.log(state.city);
+          fetchWeatherData();
+        };
+    return{handleChange,fetchDetails};
+}
+
+export default useSearchBarLogic;
