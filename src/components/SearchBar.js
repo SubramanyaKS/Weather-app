d791327bbaf9205b4cf2e83bd443d8c3197bad2c
Subramanya K S <subramanyaks2205@gmail.com>
2023-08-16 18:29:33 +0530

    seperate ui with logic

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,43 +1,15 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useContext } from 'react'
-import { useWeatherData } from '../hook/useWeatherData';
-import "../assets/main.css";
+import React from 'react'
+import SearchBarUI from './SearchBarUI'
+import useSearchBarLogic from '../hook/SearchBarLogic'
+import { useContext } from 'react';
 import { WeatherDataContext } from '../context/weatherDataContext';
 
 const SearchBar = () => {
-  const { state, dispatch } = useContext(WeatherDataContext);
-  const { fetchWeatherData } = useWeatherData(`${process.env.REACT_APP_API_LINK}/weather?q=${state.city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`);
-    const handleChange = (value) => {
-        dispatch({ type: 'SET_CITY', payload: value });
-      };
-      const fetchDetails = (event) => {
-        //const env = dotenv.config().parsed;
-        event.preventDefault();
-        // console.log(state.city);
-        fetchWeatherData();
-      };
+    const {handleChange,fetchDetails} = useSearchBarLogic();
+    const { state } = useContext(WeatherDataContext);
   return (
-    <div className="search">
-          <form onSubmit={fetchDetails}>
-            <div className="input-search">
-            <input
-            type="text"
-              placeholder="Enter the City"
-              icon={faSearch}
-              className="searchBar"
-              onChange={(e) => {
-                handleChange(e.target.value);
-              }}
-              id="outlined-basic"
-              variant="outlined"
-              label="Search"
-            />
-            <FontAwesomeIcon icon={faSearch} className="icon-switch"  style={state.dark?{ color:"#00ffff"}:{color:"#000"}} />
-            </div>
-          </form>
-        </div>
+    <SearchBarUI handleChange={handleChange} state={state} fetchDetails={fetchDetails}/>
   )
 }
 
-export default SearchBar
+export default SearchBar;
