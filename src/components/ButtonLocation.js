fc010d6fa91ae6fd6b18bc537cfd7b3bb3da104e
Subramanya K S <subramanyaks2205@gmail.com>
2023-08-16 18:29:15 +0530

    sperate ui with logic

--- src/components/ButtonLocation.js
+++ src/components/ButtonLocation.js
@@ -1,38 +1,11 @@
-import React, { useContext, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
-import { useWeatherData } from '../hook/useWeatherData';
-import { WeatherDataContext } from '../context/weatherDataContext';
-
-const API_URL = `${process.env.REACT_APP_API_LINK}/weather`;
+import React from 'react';
+import useButtonLocationLogic from '../hook/ButtonLocationLogic';
+import ButtonLocationUI from './ButtonLocationUI';
 
 const ButtonLocation = () => {
-  const { state, dispatch } = useContext(WeatherDataContext);
-  const { fetchWeatherData } = useWeatherData(`${API_URL}?lat=${state.latitude}&lon=${state.longitude}&units=metric&appid=${process.env.REACT_APP_API_KEY}`);
+  const { handleFetchWeather } = useButtonLocationLogic();
 
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          dispatch({ type: 'SET_LAT', payload: latitude });
-          dispatch({ type: 'SET_LON', payload: longitude });
-        },
-        (error) => {
-          console.error('Error getting user location:', error);
-        }
-      );
-    } else {
-      console.log("Geolocation not available");
-    }
-  }, []); // Empty dependency array means this effect runs once on mount
-
-  const fetchWeather = () => {
-    fetchWeatherData();
-  }
-
-  return (
-    <Button className='btn btn-primary' onClick={fetchWeather}>Current Location</Button>
-  );
-}
+  return <ButtonLocationUI handleFetchWeather={handleFetchWeather} />;
+};
 
 export default ButtonLocation;
