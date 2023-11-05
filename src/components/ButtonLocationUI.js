efdea47d6493a95957f761bb3754a0c4372f0fa7
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-05 09:09:00 +0530

    updated icon

--- src/components/ButtonLocationUI.js
+++ src/components/ButtonLocationUI.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../assets/main.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocation
+} from "@fortawesome/free-solid-svg-icons";
 
 const ButtonLocationUI = ({ state,handleFetchWeather }) => {
   return (
     <>
-      <button className={state.dark?"button button-dark":"button button-light"}  onClick={handleFetchWeather}><b>Current Location</b></button>
+      <button className={state.dark?"button button-dark":"button button-light"}  onClick={handleFetchWeather}><b>Current Location <FontAwesomeIcon icon={faLocation}/></b></button>
     </>
   );
 };
