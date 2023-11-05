fa868472832a00c326d92fa70f15e888cf934bae
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-05 07:02:51 +0530

    updated the mini card

--- src/components/WeatherMiniCard.js
+++ src/components/WeatherMiniCard.js
@@ -7,7 +7,7 @@ import { WeatherDataContext } from '../context/weatherDataContext';
 const MiniCard = ({icon,text,cdata,unit}) => {
   const {state} = useContext(WeatherDataContext);
   return (
-    <Card className={state.dark?"card-dark":"card-light"} style={{ width: "60%" }}>
+    <Card className={state.dark?"card-dark":"card-light"} style={{ width: "60%" ,backgroundColor:"transparent"}}>
         <Card.Text>
           <FontAwesomeIcon icon={icon} fade /> <b>{text} </b>{" "}
         </Card.Text>
