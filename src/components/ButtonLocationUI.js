a6808ddbb151e790d75cd6630c7f8bd87c866cdc
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-19 11:05:34 +0530

    added location button to search bar

--- src/components/ButtonLocationUI.js
+++ src/components/ButtonLocationUI.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import '../assets/main.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLocation
-} from "@fortawesome/free-solid-svg-icons";
+import {faLocationDot} from "@fortawesome/free-solid-svg-icons";
 
-const ButtonLocationUI = ({ state,handleFetchWeather }) => {
+const ButtonLocationUI = ({ handleFetchWeather }) => {
   return (
-    <>
-      <button className={state.dark?"button button-dark":"button button-light"}  onClick={handleFetchWeather}><b>Current Location <FontAwesomeIcon icon={faLocation}/></b></button>
-    </>
+      <button className="button"  onClick={handleFetchWeather}><b> <FontAwesomeIcon icon={faLocationDot}/></b></button>
   );
 };
 
