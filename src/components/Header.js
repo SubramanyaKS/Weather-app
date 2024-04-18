5fa7ad5eeefd8984692401687e6b36b02ca0339f
Subramanya K S <subramanyaks2205@gmail.com>
2024-04-18 06:40:31 +0530

    used reactjs-toggleswitch package

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React, { useContext } from 'react'
 import { Container, Navbar } from 'react-bootstrap'
 import "../assets/main.css";
-import Switch from "react-switch";
 import { WeatherDataContext } from '../context/weatherDataContext';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import {ToggleSwitch} from 'reactjs-toggleswitch';
 const Header = () => {
     const { state,dispatch} = useContext(WeatherDataContext);
     console.log(state.dark);
@@ -22,7 +21,7 @@ const Header = () => {
     <Navbar bg={state.dark?"dark":"light"} variant={state.dark?"dark":"light"}>
         <Container>
           <Navbar.Brand href="#home">Weather App</Navbar.Brand>
-          <Switch checked={state.dark} onHandleColor="#000" offColor='#add8e6' onColor="#131862" checkedIcon={<FontAwesomeIcon style={{color:"#fff",padding:6}} icon={faMoon}/>}  uncheckedIcon={<FontAwesomeIcon style={{color:"#ffa500",padding:6}} icon={faSun}/>} onChange={()=>changeTheme()}/>
+        <ToggleSwitch checked={state.dark} onToggle={changeTheme} thumbOnColor={'#000'} onColor="#131862" offColor='#add8e6'><FontAwesomeIcon style={{color:!state.dark?"#ffa500":"#fff"}} icon={!state.dark ? faSun : faMoon} /></ToggleSwitch>
         </Container>
       </Navbar>
   )
