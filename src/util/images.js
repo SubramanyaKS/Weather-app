819ee4cdb4f283c3c69a68d3fe523abc4f316757
Subramanya K S <subramanyaks2205@gmail.com>
2024-08-31 11:21:46 +0530

    added utils

---
+++ src/util/images.js
@@ -0,0 +1,11 @@
+import defaultImg from '../assets/img/default.jpg';
+import clearImg from '../assets/img/clear.jpg';
+import snowImg from '../assets/img/snow.jpg';
+import rainImg from '../assets/img/rain.jpg';
+import cloudImg from '../assets/img/cloud.jpg';
+import smokeImg from '../assets/img/smoke.jpg';
+import fogImg from '../assets/img/fog.jpg';
+
+export const weatherimage = {
+defaultImg,clearImg,snowImg,rainImg,cloudImg,smokeImg,fogImg    
+}
